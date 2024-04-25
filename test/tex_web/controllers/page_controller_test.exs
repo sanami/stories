910da795d5ae91779b816bc75e2cba9e03f7906f
sa <sanami77@gmail.com>
2024-04-25 17:43:37 +0500

    Fix test

--- test/tex_web/controllers/page_controller_test.exs
+++ test/tex_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule TexWeb.PageControllerTest do
   use TexWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "home"
+  test "GET /test", %{conn: conn} do
+    conn = get(conn, ~p"/test")
+    assert html_response(conn, 200) =~ "test"
   end
 end
